928c50b12e13fc3c204a626876c60dcf761134a3
Huzaifa100203 <huzefafm53786@gmail.com>
2025-08-16 12:15:44 +0530

    server changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Connected to MongoDB ${mongoose.connection}`);
+        console.log(`Connected to MongoDB ${mongoose.connection.name}`);
     } catch (error) {
-        console.log(error);
+        console.log("error", error);
         process.exit(1);
     }
 };
+
+module.exports = connectDB
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const authRoute = require("./routes/auth.route");
+const connectDB = require("./config/config");
 const app = express();
+
+// connect DB
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("<h1>Heyy</h1>");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/api/auth", authRoute);
 
